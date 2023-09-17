8abd275874556bc9e2029bf88a1049732c25fa2f
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-09-17 13:06:41 +0100

    edit old route to ensure call routes stand correctly

---
+++ app/lib/src/home/edit_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EditPage extends StatelessWidget {
+  const EditPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LISTINHA'),
+      ),
+    );
+  }
+}
