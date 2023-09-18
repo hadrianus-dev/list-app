5c390790c8e822b697bed8fe34f1654393659d08
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-09-18 08:12:38 +0100

    making config screen

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -14,6 +14,68 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       appBar: AppBar(
         title: const Text('LISTINHA'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configuracoes',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.system,
+              title: Text(
+                'Sistema',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              onChanged: null,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.light,
+              title: Text(
+                'Claro',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              onChanged: null,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.dark,
+              title: Text(
+                'Escuro',
+                style: Theme.of(context).textTheme.titleSmall,
+              ),
+              onChanged: null,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Controle de Dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(
+                onPressed: () {},
+                child: const Text('Apagar Cache e Reiniciar o App')),
+          ],
+        ),
+      ),
     );
   }
 }
