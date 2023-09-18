03a09ec29c3987213a560d1c752c6c832506c999
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-09-18 08:39:52 +0100

    ensure AppModular watch AppStore

--- app/lib/src/app_module.dart
+++ app/lib/src/app_module.dart
@@ -1,8 +1,14 @@
 import 'package:ListApp/src/configuration/configuration_page.dart';
+import 'package:ListApp/src/shared/stores/app_store.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ListApp/src/home/home_module.dart';
 
 class AppModule extends Module {
+  @override
+  void binds(i) {
+    i.addSingleton(AppStore.new);
+  }
+
   @override
   void routes(r) {
     r.module('/home', module: HomeModule());
