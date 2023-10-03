9531147f92b153ded66974327a619a41113ed6d9
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-10-03 11:45:20 +0100

    tranform app in statefull widget

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,22 +1,34 @@
+import 'package:ListApp/src/configuration/service/configuration_service.dart';
 import 'package:ListApp/src/shared/stores/app_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:ListApp/src/shared/themes/themes.dart';
+import 'package:rx_notifier/rx_notifier.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final config = Modular.get<ConfigurationService>();
+  final appStore = Modular.get<AppStore>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Modular.setInitialRoute('/home/');
-    final appStore = context.watch<AppStore>(
-      (store) => store.themeMode,
-    );
+    final themeMode = context.select(() => appStore.themeMode.value);
     return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      themeMode: appStore.themeMode.value,
+      themeMode: themeMode,
       theme: lightTheme,
       darkTheme: darkTheme,
       routerDelegate: Modular.routerDelegate,
